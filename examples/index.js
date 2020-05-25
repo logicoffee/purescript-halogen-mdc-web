7ed73ff8c0cb3bb2eb7d5c2d57bbc70fc1b1aced
logicoffee <riemann1618@gmail.com>
2020-05-25 16:53:45 +0900

    Improve Button demos

--- examples/index.js
+++ examples/index.js
@@ -1,14 +1,15 @@
 import Main from '../output/Main'
+import './style.scss'
 
 function main() {
-    Main.main()
+  Main.main()
 }
 
 if (module.hot) {
-    module.hot.accept(() => {
-        console.log('Reloaded, running main again')
-        main()
-    })
+  module.hot.accept(() => {
+    console.log('Reloaded, running main again')
+    main()
+  })
 }
 
 console.log('Starting App')
