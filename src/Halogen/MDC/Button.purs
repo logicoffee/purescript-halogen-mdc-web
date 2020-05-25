899b499b9c7eedef8def076d974a75806a5d691a
logicoffee <riemann1618@gmail.com>
2020-05-25 12:16:48 +0900

    Improve button implementation

--- src/Halogen/MDC/Button.purs
+++ src/Halogen/MDC/Button.purs
@@ -3,13 +3,15 @@ module Halogen.MDC.Button where
 import Prelude
 
 import Data.Maybe (Maybe (..))
-import Data.Array ((:), concat)
+import Data.Tuple.Nested ((/\))
 
 import Halogen.HTML as HH
 import Halogen.HTML.Properties as HP
 import Halogen.HTML.Properties.ARIA as ARIA
 import Halogen.HTML.Events as HE
 import Web.UIEvent.MouseEvent (MouseEvent)
+
+import Halogen.MDC.Theme as Th
 
 type Props a =
   { variant :: Variant
@@ -18,6 +20,8 @@ type Props a =
   , ripple :: Boolean
   , disabled :: Boolean
   , onClick :: MouseEvent -> Maybe a
+  , theme :: Th.Theme
+  , extraClasses :: Array HH.ClassName
   }
 
 data Variant
@@ -38,44 +42,78 @@ defaultProps =
   , ripple: true
   , disabled: false
   , onClick: const Nothing
+  , theme: Th.Primary
+  , extraClasses: []
   }
 
 button :: forall w i. Props i -> HH.HTML w i
 button props =
   HH.button
-    [ HP.classes $ toClassNames props.variant
+    [ HP.classes $ rootClassNames props <> props.extraClasses
     , HE.onClick props.onClick
     , HP.disabled props.disabled
     ]
-    $ concat
-      [ if props.ripple
-          then [ HH.div [ HP.class_ $ HH.ClassName "mdc-button__ripple" ] [] ]
-          else []
-      , content
-      ]
+    $ ripple <> content
 
   where
-    toClassNames :: Variant -> Array HH.ClassName
-    toClassNames variant =
-      HH.ClassName "mdc-button"
-      : case variant of
-          Text -> []
-          Raised -> [ HH.ClassName "mdc-button--raised" ]
-          Unelevated -> [ HH.ClassName "mdc-button--unelevated" ]
-          Outlined -> [ HH.ClassName "mdc-button--outlined" ]
+    ripple :: Array (HH.HTML w i)
+    ripple = if props.ripple
+      then [ HH.div [ HP.class_ cls.buttonRipple ] [] ]
+      else []
 
     content :: Array (HH.HTML w i)
     content = case props.icon of
       Nothing -> [label]
       Just (Before iconName) -> [icon iconName, label]
       Just (After iconName) -> [label, icon iconName]
-      where
-        label = HH.span
-          [ HP.class_ $ HH.ClassName "mdc-button__label" ]
-          [ HH.text props.label ]
-        icon = \iconName -> HH.i
-          [ HP.classes $
-              map HH.ClassName [ "mdc-button__icon", "material-icons" ]
-          , ARIA.hidden "true"
-          ]
-          [ HH.text iconName ]
+
+    label = HH.span
+      [ HP.classes [ cls.buttonLabel ] ]
+      [ HH.text props.label ]
+    icon = \iconName -> HH.i
+      [ HP.classes [ cls.buttonIcon, HH.ClassName "material-icons" ]
+      , ARIA.hidden "true"
+      ]
+      [ HH.text iconName ]
+
+rootClassNames :: forall a. Props a -> Array HH.ClassName
+rootClassNames props = case props.variant of
+  Text -> [ cls.button ]
+    <> case props.disabled /\ props.theme of
+      true /\ _ -> []
+      _ /\ Th.Primary -> [ Th.cls.themePrimary ]
+      _ /\ Th.Secondary -> [ Th.cls.themeSecondary ]
+  Raised -> [ cls.button, cls.buttonRaised ]
+    <> case props.disabled /\ props.theme of
+      true /\ _ -> []
+      _ /\ Th.Primary -> [  Th.cls.themePrimaryBg, Th.cls.themeOnPrimary ]
+      _ /\ Th.Secondary -> [ Th.cls.themeSecondaryBg, Th.cls.themeOnSecondary ]
+  Unelevated -> [ cls.button, cls.buttonUnelevated ]
+    <> case props.disabled /\ props.theme of
+      true /\ _ -> []
+      _ /\ Th.Primary -> [ Th.cls.themePrimaryBg, Th.cls.themeOnPrimary ]
+      _ /\ Th.Secondary -> [ Th.cls.themeSecondaryBg, Th.cls.themeOnSecondary ]
+  Outlined -> [ cls.button, cls.buttonOutlined ]
+    <> case props.disabled /\ props.theme of
+      true /\ _ -> []
+      _ /\ Th.Primary -> [ Th.cls.themePrimary ]
+      _ /\ Th.Secondary -> [ Th.cls.themeSecondary ]
+
+cls ::
+  { button :: HH.ClassName
+  , buttonRipple :: HH.ClassName
+  , buttonRaised :: HH.ClassName
+  , buttonUnelevated :: HH.ClassName
+  , buttonOutlined :: HH.ClassName
+  , buttonLabel :: HH.ClassName
+  , buttonIcon :: HH.ClassName
+  }
+cls =
+  { button: HH.ClassName "mdc-button"
+  , buttonRipple: HH.ClassName "mdc-button__ripple"
+  , buttonRaised: HH.ClassName "mdc-button--raised"
+  , buttonUnelevated: HH.ClassName "mdc-button--unelevated"
+  , buttonOutlined: HH.ClassName "mdc-button--outlined"
+  , buttonLabel: HH.ClassName "mdc-button__label"
+  , buttonIcon: HH.ClassName "mdc-button__icon"
+  }
